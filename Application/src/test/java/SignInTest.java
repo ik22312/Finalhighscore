ebf704a5bb5251185b7d9813f06834d192a9e12a
Your Name <you@example.com>
2018-05-26 13:55:41 -0400

    abc

---
+++ Application/src/test/java/SignInTest.java
@@ -0,0 +1,39 @@
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class SignInTest extends FSBase {
+
+    @Test
+    public void testFSSignIn() throws InterruptedException {
+        //fsLogin();
+        //Assert.assertEquals(driver.getTitle(), "Message Inbox | Secure Message Center");
+        Thread.sleep(5000);
+        driver.findElement(By.xpath("//*[@id=\"twotabsearchtextbox\"]")).sendKeys("deal of the day");
+        Thread.sleep(5000);
+        driver.findElement(By.xpath("//*[@id=\"nav-search\"]/form/div[2]/div/input")).click();
+        Thread.sleep(5000);
+    }
+
+
+//    @Test
+//    public void myTestCase2() {
+//
+//        // test step 1
+//        // test step 2
+//        // test step 3
+//        // test step 4
+//
+//    }
+//
+//    @Test
+//    public void myTestCase4() {
+//
+//        // test step 1
+//        // test step 2
+//        // test step 3
+//        // test step 4
+//    }
+
+
+}
